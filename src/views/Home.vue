e0870f2211c2c26ffe247b43fdb2aab70ce7a776
Kevin <nelson67@stolaf.edu>
2019-09-16 21:25:42 -0500

    add range slider to ajust num of columns

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="home">
+    <range-slider
+      class="slider"
+      min="10"
+      max="800"
+      step="10"
+      v-model="numOfColumns"
+      @change="changeWidthOfColumns()">
+    </range-slider>
+    <div>
+      {{ numOfColumns }}
+    </div>
     <div class="row">
       <div class="row-container">
-        <div v-for="column in columns" v-bind:key="column">
-          <div class="column" :style="divDimensions(10, column.length)">
+        <div v-for="column in columns">
+          <div class="column" :style="divDimensions(widthOfColumns, column.length)">
           </div>
         </div>
       </div>
@@ -13,18 +24,24 @@
 
 <script>
 
+import RangeSlider from 'vue-range-slider'
+// you probably need to import built-in style
+import 'vue-range-slider/dist/vue-range-slider.css'
+
 export default {
   name: 'home',
   components: {
+    RangeSlider
   },
   created() {
     for(var i = 0; i < this.numOfColumns; i++) {
-      this.columns.push({ length: this.randomNumberBetween(100, 1000) })
+      this.columns.push({ length: this.randomNumberBetween(0, 800) })
     }
   },
   data() {
     return {
-      numOfColumns: 100,
+      numOfColumns: 800,
+      widthOfColumns: 1,
       columns: []
     }
   },  
@@ -36,6 +53,13 @@ export default {
       min = Math.ceil(min);
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+    changeWidthOfColumns() {
+      console.log("changing");
+      this.widthOfColumns = 800 / this.numOfColumns;
+    },
+    createColumns() {
+
     }
   }
 }
@@ -44,7 +68,8 @@ export default {
 <style>
 
 .row {
-  max-width: 800px;
+  /* background: black; */
+  width: 800px;
   margin: 0px auto;
   overflow: hidden;
 }
@@ -54,9 +79,8 @@ export default {
 }
 
 .column {
-  background: blue;
-  /* height: 100px; */
-  /* width: 100px; */
+  background: black;
+  /* border-left: 1px solid black; */
 }
 
 </style>
