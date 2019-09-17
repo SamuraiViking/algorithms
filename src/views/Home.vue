403f0f81983ebff08761092c120b2bd909f387c0
Kevin <nelson67@stolaf.edu>
2019-09-16 19:10:16 -0500

    add columns

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <div v-for="column in columns" v-bind:key="column">
+        <div class="column" :style="divDimensions(200, column.length)">
+          {{ column.length }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      columns: [
+        { length: 500 },
+        { length: 100 },
+        { length: 500 },
+        { length: 200 },
+        { length: 500 },
+        { length: 300 },
+      ]
+    }
+  },
+  methods: {
+    divDimensions(width, height) {
+      return `width: ${width}px; height: ${height}px;`
+    }
   }
 }
 </script>
+
+<style>
+
+.row {
+  display: flex;
+}
+
+.column {
+  background: blue;
+  /* height: 100px; */
+  /* width: 100px; */
+}
+
+</style>
